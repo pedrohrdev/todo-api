1a2aa1e92435dd9ddc022a68f78b387d233bd44a
pedrohrdev <pedrohrdev@gmail.com>
2026-05-06 08:05:56 -0300

    feat: initial setup with express and supabase connection

---
+++ src/server.ts
@@ -0,0 +1,63 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import { supabase } from './lib/supabase';
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+// Conection test
+app.get('/health', async (req, res): Promise<void> => {
+
+    const { data, error } = await supabase.from('users').select('count');
+    
+    if(error) {
+
+        res.status(500).json(
+            {
+                error: error.message
+            }
+        )
+
+        return
+
+    };
+
+    res.json(
+        {
+            ok: true, data
+        }
+    );
+
+});
+
+// Search for all active tasks
+app.get('/tasks', async (req, res): Promise<void> => {
+
+    const { data, error } = await supabase
+        .from('tasks')
+        .select('*, users(email)')
+        .eq('is_active', true);
+
+        if(error) {
+            res.status(500).json(
+                {
+                    error: error.message
+                }
+            )
+
+            return
+        };
+
+        res.json(data)
+
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+
+    console.log(`Servidor rodando na porta ${PORT}`)
+
+});
