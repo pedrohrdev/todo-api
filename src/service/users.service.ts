9a32e4732bcaa68e2e1f6e432eda60044044a7a3
pedrohrdev <pedrohrdev@gmail.com>
2026-05-12 08:19:26 -0300

    feat: add AppError class for service layer error handling

--- src/service/users.service.ts
+++ src/service/users.service.ts
@@ -5,6 +5,14 @@ export async function createUserService(name: string, email: string, password: s
     // hash the password, and save the user to the database.
     // This is just a placeholder implementation.
 
+    if(email === "") {
+        throw new Error('Email cannot be empty');
+    } else if(password === "") {
+        throw new Error('Password cannot be empty');
+    } else if (name.length < 3) {
+        throw new Error('Name must be at least 3 character long');
+    }
+
     
 
 }   
