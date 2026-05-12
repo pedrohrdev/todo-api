a53ec5ae333d03005b111ae936a11dda09c939f7
pedrohrdev <pedrohrdev@gmail.com>
2026-05-12 08:06:53 -0300

    feat: Adding descriptive comments to the user controller, in the createUser function.

--- src/controllers/users.controllers.ts
+++ src/controllers/users.controllers.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import { createUserService } from "../service/users.service";
 
+// Register a new user
 export async function createUser(req: express.Request, res: express.Response) {
 
     // retrieving the data
@@ -22,14 +23,17 @@ export async function createUser(req: express.Request, res: express.Response) {
 
     try {
 
+        // Here, we call the servicve to create the user
         await createUserService(name, email, password);
 
+        // If the user is created successfully, we return a success message
         return res.status(201).json(
             {
                 message: "Created user sucessfuly!"
             }
         )
 
+    // If there's an error, we catch it and return a 500 status with the error message    
     } catch(error) {
 
         return res.status(500).json(
