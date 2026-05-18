fea7b397fbdd5be7a8f0abfd516fad7db8e321bd
pedrohrdev <pedrohrdev@gmail.com>
2026-05-18 07:56:45 -0300

    feat: configuring the login route

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,11 +1,15 @@
 import express from 'express';
 
 import { createUser } from '../controllers/users.controllers';
+import { loginUser } from '../controllers/users.controllers';
 
 // Create a new router instance
 const router = express.Router();
 
 // Route to create a new user
 router.post('/', createUser);
+
+// Route to login a user
+router.post('/', loginUser)
 
 export default router;
