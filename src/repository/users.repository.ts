4235cccd97240de3dd85e5cfac948e3ced15047a
Moisés Figueiredo <moisesfigueiredo2508@gmail.com>
2026-05-25 16:27:42 -0300

    fix: Structure adding repository layer

---
+++ src/repository/users.repository.ts
@@ -0,0 +1,35 @@
+import { supabase } from "../lib/supabase";
+
+async function checkExistingUser(email: string){
+    const {data} = await supabase
+        .from('users')
+        .select('id')
+        .eq('email', email)
+        .maybeSingle(); 
+
+    return data
+}
+
+async function createUser(name: string, email: string, hashedPassword: string) {
+    const { data } = await supabase
+        .from('users')
+        .insert([
+            { name, email, password_hash: hashedPassword }
+        ]);
+
+    return data
+}
+
+async function loginUser(email: string, password: string) {
+     const { data } = await supabase
+        .from('users')
+        .select('*')
+        .eq('email', email)
+        .maybeSingle();
+
+    return data
+
+}
+
+
+export default {checkExistingUser, createUser, loginUser}
