dbf40c978c63fcddb17a6562590aa6bf0aa0d674
pedrohrdev <pedrohrdev@gmail.com>
2026-06-01 08:16:58 -0300

    (refactor): Remove the empty data check from the controller, as we added validators that will use a schema for checking, since in practice the data already arrives clean in the controller.

--- src/controllers/users.controllers.ts
+++ src/controllers/users.controllers.ts
@@ -9,16 +9,6 @@ export async function createUser(req: express.Request, res: express.Response) {
     // retrieving the data
     const { name, email, password } = req.body;
 
-    // If the data isn't here, we'll return this message
-    if(!name || !email || !password) {
-
-        return res.status(400).json(
-            {
-                message: "Email and password and name are required"
-            }
-        );
-    }
-
     // Here, we'll call the service to
     // createUser, so, the data is ok to insert,
     // so, let's make it
