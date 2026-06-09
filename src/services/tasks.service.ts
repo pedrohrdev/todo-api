36a47e4495ebfb6f186a5b8defa9472c9dcd5575
pedrohrdev <pedrohrdev@gmail.com>
2026-06-09 09:25:02 -0300

    (feat): Creating the service layer for the general task listing.

--- src/services/tasks.service.ts
+++ src/services/tasks.service.ts
@@ -20,4 +20,10 @@ async function updateTaskService(
 
 };
 
-export const taskService = { createTaskService, updateTaskService }
+async function getTasksService(userId: number) {
+
+    return tasksRepository.getTasks(userId);
+
+}
+
+export const taskService = { createTaskService, updateTaskService, getTasksService }
